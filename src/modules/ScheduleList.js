23892bd7481764686c57db82b79ac1f0b4860e1c
Rob Carling <robcarling@Robs-MacBook-Pro-2.local>
2020-02-05 17:56:08 -0500

    Updates to Speakers and Schedule sections to make content dynamic

--- src/modules/ScheduleList.js
+++ src/modules/ScheduleList.js
@@ -2,100 +2,96 @@ import React, { Component } from 'react';
 import { graphql, StaticQuery } from "gatsby"
 
 
-export default class Jumbotron extends Component {
+export default props => (
+    <StaticQuery
+        query = {graphql `
+          query ScheduleQuery {
+            allAgilityContentEventSession {
+              nodes {
+                myFields {
+                  timeRange
+                  topicDescription
+                  topicTitle
+                  speaker {
+                    fields {
+                      companyName
+                      headshot {
+                        url
+                      }
+                      jobTitle
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }                         
+        `}
+        render={queryData =>  {
+
+            let schedule = [];
+
+            const viewModel = {
+                item: props.item,
+                schedule: queryData.allAgilityContentEventSession.nodes
+            }
+            return(
+                <ScheduleList {...viewModel}/>  
+            );
+        }}
+    /> 
+)
+
+class ScheduleList extends Component {
+
+    renderSchedule() {
+        console.log('schedule', this.props);
+
+        if (this.props.schedule != null) {
+
+            let schedule = [];
+
+            this.props.schedule.forEach(eventSession => {
+              schedule.push(
+                <div className="content-panel schedule-item">
+                  <div className="item-info">
+                    <div className="image">
+                      <img src={eventSession.myFields.speaker.fields.headshot.url} alt={eventSession.myFields.speaker.fields.name} />
+                      <p>
+                        <strong>{eventSession.myFields.speaker.fields.name}</strong><br />
+                        {eventSession.myFields.speaker.fields.companyName}
+                      </p>
+                    </div>
+                    
+          
+                    <div className="info">
+                      <p className="time"><i class="fa fa-clock"></i> {eventSession.myFields.timeRange}</p>
+          
+                      <p className="title">{eventSession.myFields.topicTitle}</p>
+          
+                      <p>{eventSession.myFields.topicDescription}</p>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+
+            return schedule;
+        }
+    }
+
     render() {    
         return (
-        <section id="sectionSchedule" className="body-section section-schedule">
-            <div class="container">
-              <h3>
-                <i class="fad fa-clipboard-list"></i>
-                <span>Event <strong>Schedule</strong></span>
-              </h3>
-        
-              <div className="content-panel schedule-item">
-                <div className="item-info">
-                  <div className="image">
-                    <img src="https://static.agilitycms.com/agility-days-2020/speakers/mike-johnson.jpg" alt="Mike Johnson" />
-                    <p>
-                      <strong>Mike johnson</strong><br />
-                      Agility CMS
-                    </p>
-                  </div>
-                  
-        
-                  <div className="info">
-                    <p className="time"><i class="fa fa-clock"></i> 10:00am - 11:00am</p>
-        
-                    <p className="title">How JAMStack is Revolutionizing Front-end Dev</p>
-        
-                    <p>At a time of exponential changes in digital technology, speed and distraction – leading to dramatic changes in society – the Conference offers profound guidance to you, your team and your company. From 19th to 20th September 2019 we will help shift your perspective on the future and prepare you for the big digital challenges ahead.</p>
-                  </div>
-                </div>
-              </div>
-            
-              <div className="content-panel schedule-item">
-                <div className="item-info">
-                  <div className="image">
-                    <img src="https://static.agilitycms.com/agility-days-2020/speakers/mike-johnson.jpg" alt="Mike Johnson" />
-                    <p>
-                      <strong>Mike johnson</strong><br />
-                      Agility CMS
-                    </p>
-                  </div>
-                  
-                  <div className="info">
-                    <p className="time"><i class="fa fa-clock"></i> 10:00am - 11:00am</p>
-        
-                    <p className="title">How JAMStack is Revolutionizing Front-end Dev</p>
-        
-                    <p>At a time of exponential changes in digital technology, speed and distraction – leading to dramatic changes in society – the Conference offers profound guidance to you, your team and your company. From 19th to 20th September 2019 we will help shift your perspective on the future and prepare you for the big digital challenges ahead.</p>
-                  </div>
-                </div>
-              </div>
-            
-              <div className="content-panel schedule-item">
-                <div className="item-info">
-                  <div className="image">
-                    <img src="https://static.agilitycms.com/agility-days-2020/speakers/mike-johnson.jpg" alt="Mike Johnson" />
-                    <p>
-                      <strong>Mike johnson</strong><br />
-                      Agility CMS
-                    </p>
-                  </div>
-                  
-                  <div className="info">
-                    <p className="time"><i class="fa fa-clock"></i> 10:00am - 11:00am</p>
-        
-                    <p className="title">How JAMStack is Revolutionizing Front-end Dev</p>
-        
-                    <p>At a time of exponential changes in digital technology, speed and distraction – leading to dramatic changes in society – the Conference offers profound guidance to you, your team and your company. From 19th to 20th September 2019 we will help shift your perspective on the future and prepare you for the big digital challenges ahead.</p>
-                  </div>
-                </div>
-              </div>
-            
-              <div className="content-panel schedule-item">
-                <div className="item-info">
-                  <div className="image">
-                    <img src="https://static.agilitycms.com/agility-days-2020/speakers/mike-johnson.jpg" alt="Mike Johnson" />
-                    <p>
-                      <strong>Mike johnson</strong><br />
-                      Agility CMS
-                    </p>
-                  </div>
-                  
-                  <div className="info">
-                    <p className="time"><i class="fa fa-clock"></i> 10:00am - 11:00am</p>
-        
-                    <p className="title">How JAMStack is Revolutionizing Front-end Dev</p>
-        
-                    <p>At a time of exponential changes in digital technology, speed and distraction – leading to dramatic changes in society – the Conference offers profound guidance to you, your team and your company. From 19th to 20th September 2019 we will help shift your perspective on the future and prepare you for the big digital challenges ahead.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-        </section>
+            <section id="sectionSchedule" className="body-section section-schedule">
+              <div class="container">
+                <h3>
+                  <i class="fad fa-clipboard-list"></i>
+                  <span>Event <strong>Schedule</strong></span>
+                </h3>
+
+                {this.renderSchedule()}
+               </div>
+            </section>
         );
     }
 }
-
-
--- src/modules/SpeakersList.js
+++ src/modules/SpeakersList.js
@@ -2,63 +2,97 @@ import React, { Component } from 'react';
 import { graphql, StaticQuery } from "gatsby"
 
 
-export default class Jumbotron extends Component {
+export default props => (
+    <StaticQuery
+        query = {graphql `
+        query SpeakerListQuery {
+            allAgilityContentSpeaker {
+                nodes {
+                  myFields {
+                    name
+                    companyName
+                    jobTitle
+                    headshot {
+                        url
+                    }
+                  }
+                }
+              }
+          }                         
+        `}
+        render={queryData =>  {
+
+            let speakers = [];
+
+            const viewModel = {
+                item: props.item,
+                speakers: queryData.allAgilityContentSpeaker.nodes
+            }
+            return(
+                <SpeakersList {...viewModel}/>  
+            );
+        }}
+    /> 
+)
+
+class SpeakersList extends Component {
+
+    renderSpeakers() {
+        console.log('speaker', this.props);
+
+        if (this.props.speakers != null) {
+
+            let speakers = [];
+
+            this.props.speakers.forEach(speaker => {
+                speakers.push(
+                    <li key={speaker.contentID}>
+                        {speaker.myFields.headshot && 
+                            <img src={speaker.myFields.headshot.url} alt={speaker.myFields.name} />
+                        }
+                        <p>{speaker.myFields.name}</p>
+                        <p className="title">{speaker.myFields.companyName}</p>
+                    </li>
+                )
+            })
+
+            return speakers;
+        }
+    }
+
     render() {    
         return (
             <section id="sectionSpeakers" className="body-section section-speakers">
                 <div class="container">
-                <h3>
-                    <i class="fad fa-microphone-stand"></i>
-                    <span>Meet Our <strong>Speakers</strong></span>
-                </h3>
+                    <h3>
+                        <i class="fad fa-microphone-stand"></i>
+                        <span>Meet Our <strong>Speakers</strong></span>
+                    </h3>
 
-                <div className="all-speakers">
-                    <div className="speaker-info">
-                        <div class="inner">
-                        <h5>Mike Johnson</h5>
-                        <p className="title">VP of Technology at Agility CMS</p>
+                    <div className="all-speakers">
+                        <div className="speaker-info">
+                            <div class="inner">
+                            <h5>Mike Johnson</h5>
+                            <p className="title">VP of Technology at Agility CMS</p>
 
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer consequat mauris pellentesque lacinia viverra. Proin sollicitudin posuere auctor. Ut eu dignissim urna, at congue nunc. Pellentesque egestas, velit pellentesque egestas dictum, lectus sem gravida augue, consectetur congue lacus erat vel leo. Sed sed aliquet urna, sit amet suscipit elit. Aliquam ut cursus risus. </p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer consequat mauris pellentesque lacinia viverra. Proin sollicitudin posuere auctor. Ut eu dignissim urna, at congue nunc. Pellentesque egestas, velit pellentesque egestas dictum, lectus sem gravida augue, consectetur congue lacus erat vel leo. Sed sed aliquet urna, sit amet suscipit elit. Aliquam ut cursus risus. </p>
 
-                        <button className="btn" title="Get Tickets">Get Tickets</button>
+                            <button className="btn" title="Get Tickets">Get Tickets</button>
+                            </div>
+                        </div>
+
+                        <div className="speaker-list">
+                            <ul>
+                                {/* <li>
+                                    <img src="https://static.agilitycms.com/agility-days-2020/speakers/mike-johnson.jpg" alt="Mike Johnson" />
+                                    <p>Mike Johnson</p>
+                                    <p className="title">Agility CMS</p>
+                                </li> */}
+
+                                {this.renderSpeakers()}
+                            </ul>
                         </div>
                     </div>
-
-                    <div className="speaker-list">
-                        <ul>
-                        <li>
-                            <img src="https://static.agilitycms.com/agility-days-2020/speakers/mike-johnson.jpg" alt="Mike Johnson" />
-                            <p>Mike Johnson</p>
-                            <p className="title">Agility CMS</p>
-                        </li>
-                        <li>
-                            <img src="https://static.agilitycms.com/agility-days-2020/speakers/mike-johnson.jpg" alt="Mike Johnson" />
-                            <p>Mike Johnson</p>
-                            <p className="title">Agility CMS</p>
-                        </li>
-                        <li>
-                            <img src="https://static.agilitycms.com/agility-days-2020/speakers/mike-johnson.jpg" alt="Mike Johnson" />
-                            <p>Mike Johnson</p>
-                            <p className="title">Agility CMS</p>
-                        </li>
-                        <li>
-                            <img src="https://static.agilitycms.com/agility-days-2020/speakers/mike-johnson.jpg" alt="Mike Johnson" />
-                            <p>Mike Johnson</p>
-                            <p className="title">Agility CMS</p>
-                        </li>
-                        <li>
-                            <img src="https://static.agilitycms.com/agility-days-2020/speakers/mike-johnson.jpg" alt="Mike Johnson" />
-                            <p>Mike Johnson</p>
-                            <p className="title">Agility CMS</p>
-                        </li>
-                        <li>
-                            <img src="https://static.agilitycms.com/agility-days-2020/speakers/mike-johnson.jpg" alt="Mike Johnson" />
-                            <p>Mike Johnson</p>
-                            <p className="title">Agility CMS</p>
-                        </li>
-                        </ul>
-                    </div>
-                </div>
                 </div>
             </section>
         );
