b73744ad4c02c54d17aa1b4cefaeaef042b91dcd
Rob Carling <robcarling@Robs-MacBook-Pro-2.local>
2020-02-07 15:38:24 -0500

    fix for eventbrite widget

--- src/components/GlobalHeader.js
+++ src/components/GlobalHeader.js
@@ -86,7 +86,7 @@ class GlobalHeader extends Component {
                             <div className="content">
                                 <div dangerouslySetInnerHTML={this.renderHtmlContent(this.props.item.myFields.primaryContent)}></div>
 
-                                <button class="btn" id="eventbrite-widget-modal-trigger-79568644959">{this.props.item.myFields.primaryButton.text}</button>
+                                <button class="btn" id="eventbrite-widget-modal-trigger">{this.props.item.myFields.primaryButton.text}</button>
                             </div>
                         </div>
                     </div>
